d0da367004a56b9b7a039da4ebb912371ae21e70
Shubham Tandale <shubham.tandale@qualitiasoft.com>
2021-03-29 17:06:59 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const fetch = require('node-fetch')
+
+const app = express();
+const PORT = process.env.PORT || 80;
+
+const redirect = () => {
+  const urls = [
+    'https://thecbx.000webhostapp.com/health-check.php',
+  ];
+
+  return Promise.all(urls.map(url => fetch(url).then(res => res.json())));
+}
+
+app.get('/', async (req, res) => {
+  const workingUrls = await redirect();
+  workingUrls.forEach(url => {
+    res.redirect(url)
+  })
+  res.end()
+})
+
+app.listen(PORT, err => {
+  if (err) return console.log(`Error: `, err);
+  console.log(`Royaltsp-Rest-Server listening on PORT ${PORT}`);
+})
